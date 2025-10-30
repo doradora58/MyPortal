c5f0c7ffc12e68a25fc0915616c552315deff2c4
doradora58 <sheerlifechallenge@gmail.com>
2025-10-31 01:15:19 +0900

    Fix

--- frontend/my-portal/src/pages/Apps.js
+++ frontend/my-portal/src/pages/Apps.js
@@ -4,7 +4,7 @@ import { FaGithub } from 'react-icons/fa';
 export default function Apps() {
   return (
     <div style={{ textAlign: 'center' }}>
-      <h1>Welcome to the Appsss</h1>
+      <h1>My Web Apps</h1>
 
       <div style={{ display: 'flex', justifyContent: 'center', gap: '2rem', marginTop: '2rem' }}>
         {/* Gantt Chart */}
@@ -14,7 +14,18 @@ export default function Apps() {
           rel="noopener noreferrer"
           style={{ fontSize: '2rem', color: '#007bff' }}
         >
+          Gantt Chart
           <FaExternalLinkAlt title="Gantt Chart" />
+        </a>
+        {/* BBS */}
+        <a
+          href="https://ganttchart.doradora58.com"
+          target="_blank"
+          rel="noopener noreferrer"
+          style={{ fontSize: '2rem', color: '#007bff' }}
+        >
+          BBS
+          <FaExternalLinkAlt title="BBS" />
         </a>
 
         {/* GitHub */}
@@ -24,6 +35,7 @@ export default function Apps() {
           rel="noopener noreferrer"
           style={{ fontSize: '2rem', color: '#333' }}
         >
+          GitHub
           <FaGithub title="GitHub Profile" />
         </a>
       </div>
