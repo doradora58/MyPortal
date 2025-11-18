0d1d83c5d53756bcc696a53e75bd41d0bdeb102c
doradora58 <sheerlifechallenge@gmail.com>
2025-11-19 01:25:04 +0900

    Fix

--- frontend/bbs/src/main.jsx
+++ frontend/bbs/src/main.jsx
@@ -5,7 +5,7 @@ import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/bbs">
+    <BrowserRouter basename="/bbs/">
       <App />
     </BrowserRouter>
   </React.StrictMode>
